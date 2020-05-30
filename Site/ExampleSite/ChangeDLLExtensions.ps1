a2914ef42e0e7c366eb78eeec891fc8ef1746db2
zhenfan <1586052146@qq.com>
2020-05-30 22:21:12 +0800

    将wasm改成server

--- Site/ExampleSite/ChangeDLLExtensions.ps1
+++
@@ -1,4 +0,0 @@
-param([string]$filepath,[string]$tfm)
-dir $filepath\bin\Release\$tfm\wwwroot\_framework\_bin | rename-item -NewName { $_.name -replace ".dll\b",".txt" }
-((Get-Content $filepath\bin\Release\$tfm\wwwroot\_framework\blazor.boot.json -Raw) -replace '.dll"','.txt"') | Set-Content $filepath\bin\Release\$tfm\wwwroot\_framework\blazor.boot.json
-Remove-Item $filepath\bin\Release\$tfm\wwwroot\_framework\blazor.boot.json.gz
